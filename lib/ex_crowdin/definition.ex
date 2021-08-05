476c13a71ef913e51371f72154656f409740eb33
Michael <mitanac@gmail.com>
2021-08-05 08:10:41 +0700

    support defining string column

--- lib/ex_crowdin/definition.ex
+++ lib/ex_crowdin/definition.ex
@@ -3,16 +3,22 @@ defmodule ExCrowdin.Definition do
   Upload provider profile's copy to Crowdin and download translations from Crowdin
   """
 
+  @default_string_ids_field  :crowdin_string_ids
+
   defmacro __using__(opts) do
     quote do
       Module.put_attribute(__MODULE__, :ex_crowdin_fields, unquote(synchronizable_fields(opts)))
       Module.put_attribute(__MODULE__, :ex_crowdin_name, unquote(synchronizable_name(opts, __CALLER__.module)))
+      Module.put_attribute(__MODULE__, :ex_crowdin_string_ids_field, unquote(synchronizable_string_ids_field(opts)))
 
       @spec __synchronize__(:fields) :: list(atom)
       def __synchronize__(:fields), do: @ex_crowdin_fields
 
       @spec __synchronize__(:name) :: binary
       def __synchronize__(:name), do: @ex_crowdin_name
+
+      @spec __synchronize__(:name) :: atom
+      def __synchronize__(:string_ids_field), do: @ex_crowdin_string_ids_field
     end
   end
 
@@ -32,6 +38,15 @@ defmodule ExCrowdin.Definition do
     case Keyword.fetch(opts, :name) do
       :error -> name_from_module(caller_module)
       {:ok, name} -> name
+    end
+  end
+
+  defp synchronizable_string_ids_field(opts) do
+    case Keyword.fetch(opts, :string_ids_field) do
+      {:ok, string_ids_field} when is_atom(string_ids_field) ->
+        string_ids_field
+
+      _ -> @default_string_ids_field
     end
   end
 
